5476a82182b801a6340b759e33361bf3df5137c4
ryankimani-norman <ryannorman396@gmail.com>
2024-10-15 10:45:44 +0300

    Add files via upload

---
+++ area.c
@@ -0,0 +1,19 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    // measurements
+    int length;
+    int width;
+    int area;
+    // enter length
+    ;printf("Enter a length",length);
+    ;scanf("%d",&length);
+// enter width
+  ;printf("Enter a width",width);
+   ;scanf("%d",&width);
+   // area is length by width
+   area=length*width
+  ;printf("/n Area of the rectangle is:%d",area);
+    return (0);
+}
---
+++ grading.c
@@ -0,0 +1,23 @@
+// Grading
+#include <stdio.h>
+
+int main(){
+    int kisw,biology,maths;
+    
+    printf("Enter subject kisw,");
+    scanf("%d",&kisw);
+    
+   printf("Enter subject biology,");
+   scanf("%d",&biology);
+   
+   printf("Enter subject maths,");
+   scanf("%d",&maths);
+   int total_marks, average_marks;
+   printf("The total_marks,%d\n",total_marks);
+    average_marks=total_marks/3;
+    
+    if(average_marks>=70, && average<=100)
+      
+   
+    return 0;
+}
