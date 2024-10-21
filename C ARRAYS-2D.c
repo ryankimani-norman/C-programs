6373ac86a9fdb16f94d208ff870746a2c866cc9a
ryankimani-norman <ryannorman396@gmail.com>
2024-10-21 13:30:51 +0300

    Add files via upload

---
+++ C ARRAYS-2D.c
@@ -0,0 +1,17 @@
+// C Arrays 2D
+#include <stdio.h>
+
+int main() {
+  int i,j;
+  int marks[2][3]={
+      {40,50,60},
+      {70,80,90};
+      for(i=0;i<2;i++){
+          for(j=0;j<3;j++){
+              printf("marks[%d][%d]=%d\n",i,j,marks[i][j]);
+          }
+      
+
+
+    return 0;
+}
---
+++ C array-1D.c
@@ -0,0 +1,11 @@
+//C ARRAYS-1D
+#include <stdio.h>
+
+int main() {
+int i;
+int marks[6]={40,50,60,70,80,90};
+for(i=0;i<6;i++){
+    printf("%d,\n",marks[i]);
+}
+    return 0;
+}
